bb7f72590c5d0c3622c329871d6d07ab66833e2f
williamvg93 <sonico_5071@hotmail.com>
2023-11-23 11:02:03 -0500

    feat: :sparkles: Add ApplicationServiceExtensions on Extensions, Add AppUse on program

--- ApiGardens/Program.cs
+++ ApiGardens/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using ApiGardens.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Data;
 
@@ -9,6 +11,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+/* Add AddAPlicationServices */
+builder.Services.AddAplicationServices();
+
+/* Add Cors */
+builder.Services.ConfigureCors();
+
+/* Add AutoMApper */
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
+
+
 
 /* Add connection to database */
 builder.Services.AddDbContext<ApiGardensContext>(options =>
@@ -43,6 +58,9 @@ using (var scope = app.Services.CreateScope())
         _logger.LogError(ex, "Ocurrio un error durante la migracion !!");
     }
 }
+
+/* Use Cors */
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
